b1f32326af59958d53fe029fcbc6f34279233364
github username <github email address>
2018-06-25 14:30:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file
+findMatching = (drivers,name) => {
+  return drivers.filter(person => person.toLowerCase() === name.toLowerCase())
+  // debugger
+}
+
+fuzzyMatch = (drivers,input) => {
+  const filterInputs = drivers.filter(person => {
+    if (person.slice(0,input.length) === input) {
+      // debugger;
+      return person; 
+    }
+  })
+  return filterInputs; 
+}
+
+matchName = (drivers,personName) => {
+  const nameMatch = drivers.filter(person => {
+    if (person.name === personName) {
+      return person; 
+    }
+  })
+  // debugger
+  return nameMatch; 
+}
