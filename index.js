ca6a12a04f5c5abeeef8994bb0d5ea34831e1df2
Alberto Carreras <a.carreras.c@gmail.com>
2018-06-24 21:08:16 -0400

    Solved lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+function findMatching(array, string) {
+
+   return array.filter(function (element) { return element.toLowerCase() === string.toLowerCase(); });
+
+}
+
+
+function fuzzyMatch(array, string) {
+
+   return array.filter(function (element) { return element.slice(0,string.length).toLowerCase() === string.toLowerCase(); });
+}
+
+
+function matchName(array, string) {
+
+   return array.filter(function (element) { return element.name.toLowerCase() === string.toLowerCase(); });
+
+}
