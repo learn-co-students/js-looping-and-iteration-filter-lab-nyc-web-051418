c3633a4c2a04316f1578b6aa6bc987b0043a66a9
squamuglia <maxsmouha@gmail.com>
2018-06-25 13:54:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file
+function findMatching(list, string) {
+  return list.filter(function (name) {
+    return name.toLowerCase() === string.toLowerCase();
+  });
+}
+
+function fuzzyMatch(list, string) {
+  return list.filter (
+    function(name) {
+      return  string[0].toLowerCase().includes(name[0].toLowerCase());
+    }
+  )
+};
+
+function matchName(list, string) {
+  return list.filter (
+    function(object) {
+      debugger;
+      return  object['name'].toLowerCase() === string.toLowerCase();
+    }
+  )
+};
